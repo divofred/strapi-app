8768f83c0744ce4597116b7d43c864e378c8d55f
divofred <fredrickemmanuelc@gmail.com>
2022-09-22 00:45:44 +0100

    update

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 const parse = require("pg-connection-string").parse;
-const config = parse(
-  "postgres://caewuvanaqgpqt:3efa58a4ef74833cc760b96507df349429d7b5642755fbb02e91884c80c0208f@ec2-54-91-223-99.compute-1.amazonaws.com:5432/dca4frai0ibhf7"
-);
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
